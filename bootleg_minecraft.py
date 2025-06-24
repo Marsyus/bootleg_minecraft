388eb0de982a0b51ce90b4d25106d67cbf9264e2
Marsyus <miadolomboy0917@gmail.com>
2025-06-24 15:05:08 +0800

    Created BootlegMinecraft class with  __init__ constructor that initializes the player along with the generation of the ground

--- bootleg_minecraft.py
+++ bootleg_minecraft.py
@@ -1,2 +1,15 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
+
+class BootlegMinecraft:
+    def __init__(self):
+        self.app = Ursina()
+        self.player = FirstPersonController()
+        Sky()
+        self.blocks = []
+
+        for rows in range(20):
+            for columns in range(20):
+                block = Button(color=color.white, model='cube', position=(columns,0,rows),
+                            texture='grass.png', parent=scene, origin_y=0.5)
+                self.blocks.append(block)
